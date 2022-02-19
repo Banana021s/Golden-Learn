4f0e094cbed17262824ff4bc7be0c552874a80ba
Banana021s <arash.k021@yahoo.com>
2022-02-19 23:44:16 +0330

    create navbar until logo of that
    add two colors in index.css, fix box-sizing, create container, fix z-index children of Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,14 @@
 import "./Header.css"
+import Navbar from './../Navbar/Navbar'
 
 function Header() {
-    return <header className="Header"></header>
+    return (
+        <header className="Header">
+            <div className="container">
+                <Navbar />
+            </div>
+        </header>
+    )
 }
 
 export default Header
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <h1 className="Nav-logo Caveat-font">Golden <span>Learn</span> </h1>
+        </nav>
+    )
+}
+
+export default Navbar
