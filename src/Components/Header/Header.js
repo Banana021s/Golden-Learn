5e0887fbbe0ac2fa64b92c623da761538ad181b1
Banana021s <arash.k021@yahoo.com>
2022-02-20 15:32:53 +0330

    Create logo of header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import "./Header.css"
 import Navbar from './../Navbar/Navbar'
+import logo from "./logo.png"
 
 function Header() {
     const titles = [ "Home", "Courses", "Blog", "Contact us" ]
@@ -8,6 +9,18 @@ function Header() {
         <header className="Header">
             <div className="container">
                 <Navbar titles={titles} />
+                <div className="Header-main-content">
+                    <div className="Header-logo-wrap">
+                        <img src={logo} className="Header-logo-wrap-overlay" alt="" />
+                        <div className="Header-logo-box">
+                            <img src={logo} alt="" />
+                        </div>
+                    </div>
+                    
+                    <div className="Header-titles">
+
+                    </div>
+                </div>
             </div>
         </header>
     )
