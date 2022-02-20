75176cf07f9f2aaedc7cd7a5c02cba71d46ae490
Banana021s <arash.k021@yahoo.com>
2022-02-20 09:25:51 +0330

    Create NavItems Component

--- src/Components/NavItems/NavItems.js
+++ src/Components/NavItems/NavItems.js
@@ -1,12 +1,13 @@
 import "./NavItems.css"
+import { Home2, Bag2, Notepad2, Call } from "iconsax-react"
 
 function NavItems() {
     return (
         <ul className="Navbar-nav">
-            <li className="Nav-item"><a href="">Home</a></li>
-            <li className="Nav-item"><a href="">Courses</a></li>
-            <li className="Nav-item"><a href="">Blog</a></li>
-            <li className="Nav-item"><a href="">Contact us</a></li>
+            <li className="Nav-item"><a href=""> <Home2 variant="Broken"/> Home      </a>  </li>
+            <li className="Nav-item"><a href=""> <Bag2 variant="Broken"/> Courses    </a>  </li>
+            <li className="Nav-item"><a href=""> <Notepad2 variant="Broken"/> Blog   </a>  </li>
+            <li className="Nav-item"><a href=""> <Call variant="Broken"/> Contact us </a>  </li>
         </ul>
     )
 }
