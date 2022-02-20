000536054869f28b6a3ed4144487af7a550ab645
Banana021s <arash.k021@yahoo.com>
2022-02-20 11:24:12 +0330

    create Navbar-details for Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import "./Navbar.css"
 import NavItems from "./../NavItems/NavItems"
+import { Profile, HambergerMenu, Login, ChartSuccess } from "iconsax-react"
 
 function Navbar() {
     const titles = [ "Home", "Courses", "Blog", "Contact us" ]
@@ -8,7 +9,20 @@ function Navbar() {
         <nav className="Navbar">
             <h1 className="Nav-logo Caveat-font">Golden <span>Learn</span> </h1>
             <NavItems titles={titles} />
-            <div></div>
+            <div className="Navbar-details">
+                <button className="user-dropdown-btn"> 
+                    <Profile color="#cdcdcd" variant="Broken" size={28}/>
+                    <ul className="user-dropdown-menu">
+                        <li className="user-dropdown-item">
+                            <a href="">Sign in <Login color="#cdcdcd" variant="Broken"/> </a>
+                        </li>
+                        <li className="user-dropdown-item">
+                            <a href="">Sign up <ChartSuccess color="#cdcdcd" variant="Broken"/></a>
+                        </li>
+                    </ul>
+                </button>
+                <button className="hamberger-menu-btn"> <HambergerMenu color="#cdcdcd" variant="Broken" size={28}/> </button>
+            </div>
         </nav>
     )
 }
