d152c6bfcbc940df0ec0482798ffe938fe76fc73
Banana021s <arash.k021@yahoo.com>
2022-02-20 08:36:22 +0330

    Create NavItems Component File
    fix align-items children of navbar, add two colors

---
+++ src/Components/NavItems/NavItems.js
@@ -0,0 +1,14 @@
+import "./NavItems.css"
+
+function NavItems() {
+    return (
+        <ul className="Navbar-nav">
+            <li className="Nav-item"><a href="">Home</a></li>
+            <li className="Nav-item"><a href="">Courses</a></li>
+            <li className="Nav-item"><a href="">Blog</a></li>
+            <li className="Nav-item"><a href="">Contact us</a></li>
+        </ul>
+    )
+}
+
+export default NavItems
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,12 @@
 import "./Navbar.css"
+import NavItems from "./../NavItems/NavItems"
 
 function Navbar() {
     return (
         <nav className="Navbar">
             <h1 className="Nav-logo Caveat-font">Golden <span>Learn</span> </h1>
+            <NavItems />
+            <div></div>
         </nav>
     )
 }
