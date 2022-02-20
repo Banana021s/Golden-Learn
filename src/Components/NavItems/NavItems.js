bbd02d5c487761afd699445d310a324858ea8522
Banana021s <arash.k021@yahoo.com>
2022-02-20 10:02:47 +0330

    Dynamic The NavItems Component

--- src/Components/NavItems/NavItems.js
+++ src/Components/NavItems/NavItems.js
@@ -1,13 +1,13 @@
 import "./NavItems.css"
 import { Home2, Bag2, Notepad2, Call } from "iconsax-react"
 
-function NavItems() {
+function NavItems(props) {
     return (
         <ul className="Navbar-nav">
-            <li className="Nav-item"><a href=""> <Home2 variant="Broken"/> Home      </a>  </li>
-            <li className="Nav-item"><a href=""> <Bag2 variant="Broken"/> Courses    </a>  </li>
-            <li className="Nav-item"><a href=""> <Notepad2 variant="Broken"/> Blog   </a>  </li>
-            <li className="Nav-item"><a href=""> <Call variant="Broken"/> Contact us </a>  </li>
+            <li className="Nav-item"><a href=""> <Home2 variant="Broken"/> { props.titles[0] } </a> </li>
+            <li className="Nav-item"><a href=""> <Bag2 variant="Broken"/> { props.titles[1] } </a> </li>
+            <li className="Nav-item"><a href=""> <Notepad2 variant="Broken"/> { props.titles[2] } </a> </li>
+            <li className="Nav-item"><a href=""> <Call variant="Broken"/> { props.titles[3] } </a> </li>
         </ul>
     )
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,10 +2,12 @@ import "./Navbar.css"
 import NavItems from "./../NavItems/NavItems"
 
 function Navbar() {
+    const titles = [ "Home", "Courses", "Blog", "Contact us" ]
+    
     return (
         <nav className="Navbar">
             <h1 className="Nav-logo Caveat-font">Golden <span>Learn</span> </h1>
-            <NavItems />
+            <NavItems titles={titles} />
             <div></div>
         </nav>
     )
