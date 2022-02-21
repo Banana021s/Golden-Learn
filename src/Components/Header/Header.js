f2f0177d3aa415c81dfaaa61cb4e48aa04079795
Banana021s <arash.k021@yahoo.com>
2022-02-21 14:06:29 +0330

    create header titles

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,8 @@
 import "./Header.css"
 import Navbar from './../Navbar/Navbar'
 import logo from "./logo.png"
+import { Bag2 } from "iconsax-react"
+import github from "./github.svg"
 
 function Header() {
     const titles = [ "Home", "Courses", "Blog", "Contact us" ]
@@ -18,7 +20,13 @@ function Header() {
                     </div>
                     
                     <div className="Header-titles">
-
+                        <h1 className="Header-logo-title">you can be a <span>gold</span></h1>
+                        <h3 className="Header-logo-title-desc">with <span>Golden</span> <span>Learn</span></h3>
+                        
+                        <div className="Header-btns">
+                            <a href="" className="see-all-courses-btn"> <Bag2 color="var(--dark-900)" variant="Broken"/> see all courses</a>
+                            <a href="https://github.com/Banana021s/Golden-Learn" target="_blank" className="github-btn"> <img src={github} alt="" /> </a>
+                        </div>
                     </div>
                 </div>
             </div>
